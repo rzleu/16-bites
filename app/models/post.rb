e72dab6030bf76444caac6920723a2bb48e6e84f
Richard Leung <richardzleung@gmail.com>
2021-09-12 22:12:28 -0700

    add photo db validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
   validates :title, :user_id, presence:  true
 
+  validate :ensure_photo
+
   belongs_to :user,
     foreign_key: :user_id,
     primary_key: :id
 
   has_one_attached :photo
+
+  def ensure_photo
+    unless self.photo.attached?
+      errors[:photo] << "must be attached"
+    end
+  end
 end
